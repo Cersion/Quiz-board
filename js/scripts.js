f6307a98a3b0187b17629b07cf2d8cb2022e5121
leon <leonmonyoro@gmail.com>
2019-04-02 10:51:33 +0300

    Changed the JavaScript code for the index.html file to function properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
-// Business (or back-end) logic:
-var add = function(number1, number2, number3, number4, number5, number6, number7, number8, number9, number10) {
-  return number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10;
-};
-
-
-
-
-// User interface (or front-end) logic:
-$(document).ready(function() {
-  $("form").submit(function(event) {
+//Business logic//
+function set(){
+  var names=document.getElementById("firstName").value;
+  var school=document.getElementById("schoolName").value;
+  document.getElementById("person").innerHTML=names;
+  document.getElementById("school").innerHTML=school;
+}
+//U.I logic//
+$(document).ready(function(){
+  $("#quiz").submit(function(event){
     event.preventDefault();
-    var number1 = parseInt($("input:radio[name=answer1]:checked").val());
-    var number2 = parseInt($("input:radio[name=answer2]:checked").val());
-    var number3 = parseInt($("input:radio[name=answer3]:checked").val());
-    var number4 = parseInt($("input:radio[name=answer4]:checked").val());
-    var number5 = parseInt($("input:radio[name=answer5]:checked").val());
-    var number6 = parseInt($("input:radio[name=answer6]:checked").val());
-    var number7 = parseInt($("input:radio[name=answer7]:checked").val());
-    var number8 = parseInt($("input:radio[name=answer8]:checked").val());
-    var number9 = parseInt($("input:radio[name=answer9]:checked").val());
-    var number10 = parseInt($("input:radio[name=answer10]:checked").val());
-    var result = add(number1, number2, number3, number4, number5, number6, number7, number8, number9, number10);
-    $("#output").text(result);
-
-    if (result > 24) {
-      $("#output2").text("Excellent")
-    } else if (result > 15) {
-      $("#output2").text("Fair")
-    } else if (result > 0) {
-      $("#output2").text("Poor, Retake test")
-    } else {
-      $("#output2").text("complete all questions")
-    }
+    var language = parseInt($("input:radio[name=Q1]:checked").val());
+    var logic = parseInt($("input:radio[name=Q2]:checked").val());
+    var method = parseInt($("input:radio[name=Q3]:checked").val());
+    var constant = parseInt($("input:radio[name=Q4]:checked").val());
+    var popup = parseInt($("input:radio[name=Q5]:checked").val());
+    var array = parseInt($("input:radio[name=Q6]:checked").val());
+    var display =parseInt((language + logic + method + constant + popup + array)/60*100);
+    var name=$("input#firstName").val();
+    $("#hide").slideDown();
+    $("input").prop("disabled", true);
+    $("#results").text(name+" "+"your score is :"+" "+display+"%");
+  });
+  $("#start").click(function(){
+    $("#quiz").slideToggle();
+  });
+  $("#name").submit(function(){
+    event.preventDefault();
+    $("#name").hide();
+    $("#begin").slideDown();
+  });
+  $("#seeResults").click(function(){
+    $("#begin").hide();
+    $("#questions").hide();
+    $("#answers").show();
   });
 });
